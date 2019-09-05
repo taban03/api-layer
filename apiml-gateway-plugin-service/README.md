c84703d09d69f52f1a04bf0ce05de12d09c6c767
pz891284 <pavlin.zahariev@broadcom.com>
2019-09-05 16:18:10 +0200

    US611898: APIML plugins - initial commit

---
+++ apiml-gateway-plugin-service/README.md
@@ -0,0 +1,2 @@
+# apiml-enabler-spring-v2-sample
+ZOWE API ML onboarding sampleservice
---
+++ apiml-gateway-plugin-service/src/main/java/com/ca/mfaas/apiml/plugin/api/v1/controller/GreetingController.java
@@ -0,0 +1,46 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+package com.ca.mfaas.apiml.plugin.api.v1.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import com.ca.mfaas.apiml.plugin.api.v1.model.Greeting;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+@Api(
+        value = "/api/v1/",
+        consumes = "application/json",
+        tags = {"The greetings API"}
+    )
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    @ApiOperation(
+            value = "Shows personalized greeting",
+            notes = "Returns personalized greetings information")
+    @ApiResponses( value = {
+            @ApiResponse(code = 200, message = "Personalized greeting response")
+    })
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
+}
